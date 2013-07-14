98e39313ed73a27a61287f43e9ab87266b89bbac
David Yip <yipdw@member.fsf.org>
2013-07-14 02:24:23 -0500

    More tightly embrace the Ember way.

--- lib/ncs_navigator/lunokhod/backend.rb
+++ lib/ncs_navigator/lunokhod/backend.rb
@@ -3,126 +3,32 @@ require 'escape_utils'
 require 'lunokhod'
 require 'lunokhod/version'
 
-require File.expand_path('../extensions', __FILE__)
-
 module NcsNavigator::Lunokhod
   class Backend
     attr_accessor :level
 
     def initialize
-      @answer = template('answer.html.erb')
-      @epilogue = template('epilogue.html.erb')
-      @grid = template('grid.html.erb')
-      @group = template('group.html.erb')
-      @label = template('label.html.erb')
+      @buffer = ""
       @prologue = template('prologue.html.erb')
-      @question = template('question.html.erb')
-      @repeater = template('repeater.html.erb')
-      @section = template('section.html.erb')
+      @epilogue = template('epilogue.html.erb')
       @survey = template('survey.html.erb')
-
-      @question_object = template('question_object.js.erb')
-
-      @in_grid = false
-      @grid_as = []
-      @grid_qs = []
-
-      @question_objects = []
     end
 
     def prologue
       @start = Time.now
-      @buffer = ""
 
       e @prologue.result(binding)
     end
 
     def epilogue
-      total = Time.now - @start
-
-      question_object_defs = @question_objects.map do |q| 
-        @question_object.result(binding)
-      end
+      duration = Time.now - @start
 
       e @epilogue.result(binding)
     end
 
     def survey(n)
-      start, finish = split_output @survey.result(binding)
-      e start
+      e @survey.result(binding)
       yield
-      e finish
-    end
-
-    def section(n)
-      start, finish = split_output @section.result(binding)
-      e start
-      yield
-      e finish
-    end
-
-    def group(n)
-      start, finish = split_output @group.result(binding)
-      e start
-      yield
-      e finish
-    end
-
-    def label(n)
-      start, finish = split_output @label.result(binding)
-      e start
-      yield
-      e finish
-    end
-    
-    def question(n)
-      if @in_grid
-        # Queue up the question for later processing by the grid generator
-        @grid_qs << n
-        yield
-      else
-        start, finish = split_output @question.result(binding)
-        e start
-        yield
-        e finish
-      end
-
-      @question_objects << n
-    end
-
-    def answer(n)
-      if @in_grid
-        # Queue up the answer for later processing by the grid generator
-        @grid_as << n
-        yield
-      else
-        q = n.question
-        start, finish = split_output @answer.result(binding)
-        e start
-        yield
-        e finish
-      end
-    end
-
-    def grid(n)
-      begin
-        @in_grid = true
-        @grid_as.clear
-        @grid_qs.clear
-
-        yield
-
-        e @grid.result(binding)
-      ensure
-        @in_grid = false
-      end
-    end
-
-    def repeater(n)
-      start, finish = split_output @repeater.result(binding)
-      e start
-      yield
-      e finish
     end
     
     def write
@@ -130,8 +36,12 @@ module NcsNavigator::Lunokhod
     end
 
     def method_missing(m, *args)
-      e "<!-- #{m} #{args.first.uuid} -->"
-      yield if block_given?
+      if args.first.respond_to?(:uuid)
+        e "<!-- #{m} #{args.first.uuid} -->"
+        yield if block_given?
+      else
+        super
+      end
     end
 
     private
@@ -146,13 +56,6 @@ module NcsNavigator::Lunokhod
 
     def template(fn)
       ERB.new(File.read(File.expand_path("../templates/#{fn}", __FILE__)))
-    end
-
-    # HACK: Lunokhod doesn't currently yield intermediate products back to
-    # other backend methods, and likely won't do so for quite a while.  This is
-    # a hack to get around that limitation.
-    def split_output(out)
-      out.split('%DATA%', 2)
     end
   end
 end
