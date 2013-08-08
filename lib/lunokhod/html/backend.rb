0656a948dfa265c996111367b0d6d2a0826c3781
David Yip <yipdw@member.fsf.org>
2013-08-08 02:00:03 -0500

    A bare-bones backend.
    Specifically, this backend just creates output directories and copies in
    Javascript files.  No survey generation yet.

--- lib/lunokhod/html/backend.rb
+++ lib/lunokhod/html/backend.rb
@@ -1,4 +1,46 @@
+require 'babosa'
+require 'fileutils'
+
 module Lunokhod::Html
   class Backend
+    include FileUtils::Verbose
+
+    attr_accessor :level
+
+    def initialize(output_dir)
+      @output_dir = output_dir
+      @javascripts_path = File.expand_path('../../../../javascripts', __FILE__)
+      @products = []
+
+      @survey_start, @survey_end = template('survey.html.erb')
+    end
+
+    def write
+      mkdir_p @output_dir
+      cp_r @javascripts_path, @output_dir
+
+      cd @output_dir do
+        @products.each do |p|
+          puts "Writing #{p.filename}"
+          File.open(p.filename, 'w') { |f| f.write(p.content) }
+        end
+      end
+    end
+
+    def method_missing(m, *args)
+      yield if block_given?
+    end
+
+    private
+
+    class Product < Struct.new(:name, :content)
+      def filename
+        name.to_slug.normalize.to_s + '.html'
+      end
+
+      def <<(str)
+        content << str
+      end
+    end
   end
 end
