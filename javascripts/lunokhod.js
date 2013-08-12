d673c1ec2987c824f3ada24eaa26d3bddbead9bb
David Yip <yipdw@member.fsf.org>
2013-08-11 23:00:02 -0500

    Just enough backend to get a selector test to fail.

---
+++ javascripts/lunokhod.js
@@ -0,0 +1,4 @@
+(function () {
+})();
+
+// vim:ts=2:sw=2:et:tw=72
--- lib/lunokhod/html/backend.rb
+++ lib/lunokhod/html/backend.rb
@@ -1,4 +1,7 @@
 require 'babosa'
+require 'erb'
+require 'escape_utils'
+require 'escape_utils/html/erb'
 require 'fileutils'
 
 module Lunokhod::Html
@@ -15,27 +18,55 @@ module Lunokhod::Html
       @survey_start, @survey_end = template('survey.html.erb')
     end
 
+    def mapping
+      @products.each_with_object({}) { |p, h| h[p.name] = p.filename }
+    end
+
+    def prologue
+    end
+
+    def epilogue
+    end
+
     def write
       mkdir_p @output_dir
       cp_r @javascripts_path, @output_dir
 
-      cd @output_dir do
-        @products.each do |p|
-          puts "Writing #{p.filename}"
-          File.open(p.filename, 'w') { |f| f.write(p.content) }
-        end
+      @products.each do |p|
+        p.output_dir = @output_dir
+        puts "Writing #{p.filename}"
+        File.open(p.filename, 'w') { |f| f.write(p.content) }
       end
     end
 
+    def survey(n)
+      @current_product = Product.new(n.name, "")
+      e @survey_start.result(binding)
+      yield
+      e @survey_end.result(binding)
+      @products << @current_product
+    end
+
     def method_missing(m, *args)
+      e "<!-- #{m} #{args.first.uuid} -->\n"
       yield if block_given?
     end
 
     private
 
-    class Product < Struct.new(:name, :content)
+    def e(str)
+      @current_product << str
+    end
+
+    def template(path)
+      data = File.read(File.expand_path("../templates/#{path}", __FILE__))
+
+      data.split('%%SPLIT%%', 2).map { |str| ERB.new(str) }
+    end
+
+    class Product < Struct.new(:name, :content, :output_dir)
       def filename
-        name.to_slug.normalize.to_s + '.html'
+        File.join(output_dir, name.to_slug.normalize.to_s + '.html')
       end
 
       def <<(str)
